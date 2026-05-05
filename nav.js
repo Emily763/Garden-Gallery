41b8f471e55bdc564d3e6266e2d20115cdf6e60e
Emily <emilyrocks08.eb@gmail.com>
2026-05-04 20:48:22 -0400

    fixed bugs and layout

--- nav.js
+++ nav.js
@@ -1,34 +1,40 @@
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+const menuBtn = document.querySelector('#menuBtn');
+const closeBtn = document.querySelector('#closeBtn');
+const mainNav = document.querySelector('#mainNav');
+
+menuBtn.addEventListener('click', () => mainNav.classList.add('open'));
+closeBtn.addEventListener('click', () => mainNav.classList.remove('open'));
+
+mainNav.querySelectorAll('a').forEach(link => {
+  link.addEventListener('click', () => mainNav.classList.remove('open'));
+});
+
+const form = document.querySelector('#contact-form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  form.reset();
+});
+
+document.querySelectorAll('[data-slider]').forEach(slider => {
+  const track = slider.querySelector('.slider-track');
+  const prev = slider.querySelector('.prev');
+  const next = slider.querySelector('.next');
+
+  function move(direction) {
+    const card = track.querySelector('.product-card');
+    const gap = 24;
+    const amount = card.offsetWidth + gap;
+    track.scrollBy({ left: direction * amount, behavior: 'smooth' });
+  }
+
+  prev.addEventListener('click', () => move(-1));
+  next.addEventListener('click', () => move(1));
+});
+
+function openMenu() {
+  document.getElementById("mainNav").classList.add("open");
 }
 
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+function closeMenu() {
+  document.getElementById("mainNav").classList.remove("open");
 }
-
-// CONTACT PAGE
-document.querySelector('#contact-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    e.target.elements.name.value = '';
-    e.target.elements.email.value = '';
-    e.target.elements.message.value = '';
-  });
-
-// END CONTACT PAGE
-
-// STORE 
-// Get the Sequence element
-var sequenceElement = document.getElementById("sequence");
-
-// Place your Sequence options here to override defaults
-// See: http://sequencejs.com/documentation/#options
-var options = {
-  animateCanvas: false,
-  phaseThreshold: false,
-  preloader: true,
-  reverseWhenNavigatingBackwards: true
-}
-
-// Launch Sequence on the element, and with the options we specified above
-var mySequence = sequence(sequenceElement, options);
-// END STORE
